7c606edb3652af26532f25b96b98b0e0d9249d54
Santiago Imelio <santiago.imelio@gmail.com>
2025-02-10 10:58:47 -0300

    finish pictureKernel code

--- chapter4/notes.md
+++ chapter4/notes.md
@@ -15,5 +15,32 @@ In general, a grid is a 3D array of blocks, and each block is a 3D array of thre
 
 Just like the indexes, the grid dimensions and block dimensions can be accessed via `gridDim` and `blockDim` respectively.
 
-## Treating multidimensional data
-The choice of 1D, 2D, or 3D thread organizations are usually based on the nature of the data.
+## Handling multidimensional data
+The choice of 1D, 2D, or 3D thread organizations are usually based on the nature of the data. For instance, grayscale images are 2D arrays of pixels, so it is often convenient to use a 2D grid of 2D blocks to process the pixels.
+
+## Matrix linearization
+When dealing with dynamically stored arrays, the C compiler does not know before hand the number of items that the array will store, and this is by design. Thus, the number of columns in a dynamically allocated 2D array is not known at compile time.
+
+As a result, programmers need to explicitly **linearize** (flatten) a dynamically allocated matrix into an equivalent 1D array.
+
+<img title="linearization" alt="Alt text" src="array-linearization.png">
+
+Let $M$ be a $m \times n$ matrix, where $M_{j,i}$ represents the element at row $j$ and column $i$.
+
+### Row-major layout access formula
+
+$$
+
+M_{j,i} = M[j * n + i]
+
+$$
+
+### Column-major access formula
+
+$$
+
+M_{j,i} = M[i * m + j]
+
+$$
+
+In reality, both static and dynamic arrays in C are linearized. Static multidimensional arrays gets to use the bracket syntax because the dimensional information is given at compile time, and under the hood the compiler linearizes it into 1D equivalent.
--- chapter4/pictureKernel.cu
+++ chapter4/pictureKernel.cu
@@ -1,11 +1,83 @@
 #include <cuda.h>
+#include <iostream>
 
-__global__ pictureKernel(float* d_Pin, float* d_Pout, int n, int m)
+using namespace std;
+
+// CUDA API error handling macro
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+__global__
+void pictureKernel(float* d_Pin, float* d_Pout, int n, int m, float factor)
 {
   int Row = blockIdx.y * blockDim.y + threadIdx.y;
   int Col = blockIdx.x * blockDim.x + threadIdx.x;
 
   if (Row < m && Col < n) {
-    d_Pout[Row * n + Col] = 2.0 * d_Pin[Row * n + Col];
+    d_Pout[Row * n + Col] = factor * d_Pin[Row * n + Col];
   }
+}
+
+void rescale(float* Pin, float* Pout, int n, int m, float factor)
+{
+  int picSize = n * m * sizeof(float);
+
+  float *d_Pin, *d_Pout;
+
+  gpuErrchk(cudaMalloc((void **) &d_Pin, picSize));
+  gpuErrchk(cudaMemcpy(d_Pin, Pin, picSize, cudaMemcpyHostToDevice));
+
+  gpuErrchk(cudaMalloc((void **) &d_Pout, picSize));
+
+  dim3 dimBlock(ceil(n/16.0), ceil(m/16.0), 1);
+  dim3 dimGrid(16, 16, 1);
+  pictureKernel<<<dimGrid, dimBlock>>>(d_Pin, d_Pout, n, m, factor);
+
+  gpuErrchk(cudaMemcpy(Pout, d_Pout, picSize, cudaMemcpyDeviceToHost));
+
+  gpuErrchk(cudaFree(d_Pin));
+  gpuErrchk(cudaFree(d_Pout));
+}
+
+float* iota(int m, int n)
+{
+  int len = m * n;
+  float* mat = new float[len];
+
+  for (int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++) {
+      mat[i * n + j] = i * n + j;
+    }
+  }
+
+  return mat;
+}
+
+int main()
+{
+  int n = 6;
+  int m = 6;
+
+  float* mat = iota(m, n);
+  float* result = new float[m * n];
+
+  rescale(mat, result, n, m, 2.0);
+
+  for (int k = 0; k < m * n; k++) {
+    if ((k + 1) % m == 0) {
+      cout << result[k] << endl;
+    } else {
+      cout << result[k] << " ";
+    }
+  }
+
+  delete mat;
+  delete result;
 }
